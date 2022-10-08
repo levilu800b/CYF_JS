4cf4397b11c48de093d54dd089e1db9d58152b79
levilu800b <levilu800b@gmail.com>
2022-10-08 16:27:13 +0100

    Second commit

--- ArrayOfObject.js
+++ ArrayOfObject.js
@@ -1,27 +1,266 @@
+// // // const cat1 = {
+// // //     name: 'Jamie',
+// // //     age: 12,
+// // // }
 
+// // // const cat2 = {
+// // //     name: 'Brave',
+// // //     age: 8,
+// // // }
 
-const cat1 = {
-    name: 'Jamie',
-    age: 12,
-}
+// // // const cat3 = {
+// // //     name: 'Sylvester',
+// // //     age: 2,
+// // // }
 
-const cat2 = {
-    name: 'Brave',
-    age: 8,
-}
+// // // const arrayOfCats = [cat1, cat2, cat3];
 
-const cat3 = {
-    name: 'Sylvester',
-    age: 2,
-}
+// // // function underFourYearsOld(cat) {
+// // //     return cat.age < 4;
+// // // }
 
-const arrayOfCats = [cat1, cat2, cat3];
+// // // const catsUnderFourYearsOld = arrayOfCats.filter(underFourYearsOld);
 
-function underFourYearsOld(cat) {
-    return cat.age < 4;
-}
+// // // console.log("Initial Cats: ", arrayOfCats);
+// // // console.log('Cats under four years old: ', catsUnderFourYearsOld);
 
-const catsUnderFourYearsOld = arrayOfCats.filter(underFourYearsOld);
+// // // EXERCISE 1
 
-console.log("Initial Cats: ", arrayOfCats);
-console.log('Cats under four years old: ', catsUnderFourYearsOld);
+// // // Define an array containing the 3 persons defined below.
+// // // Return an array of the person names (hint: use map).
+// // // Filter the persons to return an array with the person younger than 28 years old (hint: use filter).
+
+// let person1 = {
+//   name: "Alice",
+//   age: 25,
+// };
+
+// let person2 = {
+//   name: "Bob",
+//   age: 30,
+// };
+
+// let person3 = {
+//   name: "John",
+//   age: 20,
+// };
+
+// // /*
+// // DO NOT EDIT ANYTHING ABOVE THIS LINE
+// // WRITE YOUR CODE BELOW
+// // */
+
+// // // Define an array containing the 3 persons defined above
+// let persons = [person1, person2, person3]; // Complete here
+
+// // // Return an array of the person names (hint: use map)
+// let personNames = persons.map((person) => person.name); // Complete here
+
+// // // Filter the persons to return an array with the person younger than 28 years old (hint: use filter)
+// let personsYoungerThan28YearsOld = persons.filter((person) => person.age < 28); // Complete here
+
+// // /*
+// // DO NOT EDIT ANYTHING BELOW THIS LINE
+// // */
+
+// console.log(
+//   "Question 1: array defined with 3 persons -> ",
+//   persons[0] === person1 && persons[1] === person2 && persons[2] === person3
+//     ? "Passed :)"
+//     : "Not yet :("
+// );
+
+// console.log(
+//   "Question 2: array containing the person names -> ",
+//   personNames[0] === "Alice" &&
+//     personNames[1] === "Bob" &&
+//     personNames[2] === "John"
+//     ? "Passed :)"
+//     : "Not yet :("
+// );
+
+// console.log(
+//   "Question 3: array containing the persons younger than 28 years old -> ",
+//   personsYoungerThan28YearsOld[0] === person1 &&
+//     personsYoungerThan28YearsOld[1] === person3
+//     ? "Passed :)"
+//     : "Not yet :("
+// );
+
+// // EXERCISE 2
+
+// // An array of travel destinations is defined below. Each destination has a name, a distance from Glasgow, and a list of transportations available to go there.
+
+// // Filter the travelDestinations array to return all destination names reachable within 500 kms.
+// // Find a destination name reachable by ferry.
+// // Print in the console all the destination names more than 300 kms far away and reachable by train.
+
+// let destination1 = {
+// 	destinationName: 'Edinburgh',
+// 	distanceKms: 80,
+// 	transportations: ['car', 'bus', 'train'],
+// };
+
+// let destination2 = {
+// 	destinationName: 'London',
+// 	distanceKms: 650,
+// 	transportations: ['car', 'bus', 'train'],
+// };
+
+// let destination3 = {
+// 	destinationName: 'Paris',
+// 	distanceKms: 900,
+// 	transportations: ['train', 'plane'],
+// };
+
+// let destination4 = {
+// 	destinationName: 'Dublin',
+// 	distanceKms: 350,
+// 	transportations: ['plane', 'ferry'],
+// };
+
+// let travelDestinations = [
+// 	destination1,
+// 	destination2,
+// 	destination3,
+// 	destination4,
+// ];
+
+// /*
+// DO NOT EDIT ANYTHING ABOVE THIS LINE
+// WRITE YOUR CODE BELOW
+// */
+
+// // Filter the travelDestinations array to return all destination names reachable within 500 kms
+// let destinationNamesWithin500Kms = travelDestinations
+// 	.filter((destination) => destination.distanceKms < 500)
+// 	.map((destination) => destination.destinationName); // Complete here
+
+// // Find a destination name reachable by ferry
+// let destinationNameReachableByFerry = travelDestinations.find((destination) =>
+// 	destination.transportations.includes('ferry'),
+// ).destinationName; // Complete here
+
+// // Print in the console all the destination names more than 300 kms far away and reachable by train
+// let destinationNamesMoreThan300KmsAwayByTrain = travelDestinations
+// 	.filter(
+// 		(destination) =>
+// 			destination.distanceKms > 300 &&
+// 			destination.transportations.includes('train'),
+// 	)
+// 	.map((destination) => destination.destinationName); // Complete here (PRINT THE RESULT IN THE CONSOLE USING FOREACH)
+// /*
+// DO NOT EDIT ANYTHING BELOW THIS LINE
+// */
+
+// console.log(
+// 	`Question 1) Expected result: Edinburgh,Dublin, actual result: ${destinationNamesWithin500Kms}`,
+// );
+// console.log(
+// 	`Question 2) Expected result: Dublin, actual result: ${destinationNameReachableByFerry}`,
+// );
+// console.log(
+// 	`Question 3) Expected result: London,Paris, actual result: ${destinationNamesMoreThan300KmsAwayByTrain}`,
+// );
+
+// EXERCISE 3
+
+// Below are some restaurants in Glasgow. Each restaurant has a name, the number of total seats, the number of customers in the restaurant at the moment and the menu with prices.
+
+// We want to build an object to simulate a Restaurant Finder application (see below restaurantFinderApplication).
+
+// Define a method findAvailableRestaurants which takes a number of people in parameter and returns all the restaurant names which have the required number of seats available at the moment.
+
+// Define a method findRestaurantServingDish which takes a dish name in parameter and returns all the restaurant names serving this dish.
+
+// Define a method countNumberOfRestaurantsInArea which takes an area of Glasgow in parameter (center, west), and returns the number of restaurants in this area.
+
+let restaurant1 = {
+	name: 'Paesano',
+	totalSeats: 10,
+	numberOfCustomers: 8,
+	address: {
+		city: 'Glasgow',
+		area: 'center',
+	},
+	menu: ['pizza', 'calzone', 'salad'],
+};
+
+let restaurant2 = {
+	name: 'Ubiquitous Chip',
+	totalSeats: 20,
+	numberOfCustomers: 10,
+	address: {
+		city: 'Glasgow',
+		area: 'west',
+	},
+	menu: ['salad', 'chocolate cake', 'roast lamb'],
+};
+
+let restaurant3 = {
+	name: 'Monkeyz',
+	totalSeats: 15,
+	numberOfCustomers: 8,
+	address: {
+		city: 'Glasgow',
+		area: 'center',
+	},
+	menu: ['stew', 'chocolate cake', 'panini'],
+};
+
+let restaurants = [restaurant1, restaurant2, restaurant3];
+
+// /*
+// DO NOT EDIT ANYTHING ABOVE THIS LINE
+// WRITE YOUR CODE BELOW
+// */
+
+let restaurantFinderApplication = {
+	applicationName: 'Restaurant Finder',
+	applicationVersion: '1.0',
+	restaurants: restaurants,
+	findAvailableRestaurants: function (numberOfPeople) {
+		return this.restaurants
+			.filter(
+				(restaurant) =>
+					restaurant.totalSeats - restaurant.numberOfCustomers >=
+					numberOfPeople,
+			)
+			.map((restaurant) => restaurant.name);
+		// Complete here
+	},
+	findRestaurantServingDish: function (dishName) {
+		return this.restaurants
+			.filter((restaurant) => restaurant.menu.includes(dishName))
+			.map((restaurant) => restaurant.name);
+		// Complete here
+	},
+	countNumberOfRestaurantsInArea: function (area) {
+		return this.restaurants.filter(
+			(restaurant) => restaurant.address.area === area,
+		).length;
+		// Complete here
+	},
+};
+
+// /*
+// DO NOT EDIT ANYTHING BELOW THIS LINE
+// */
+
+let restaurantsAvailableFor5People =
+	restaurantFinderApplication.findAvailableRestaurants(5);
+console.log(
+	`Find available restaurants for 5 people: Expected result: Ubiquitous Chip,Monkeyz, actual result: ${restaurantsAvailableFor5People}`,
+);
+
+let restaurantsServingSalad =
+	restaurantFinderApplication.findRestaurantServingDish('salad');
+console.log(
+	`Find restaurants serving salad: Expected result: Paesano,Ubiquitous Chip, actual result: ${restaurantsServingSalad}`,
+);
+
+let numberOfRestaurantsInCityCentre =
+	restaurantFinderApplication.countNumberOfRestaurantsInArea('center');
+console.log(
+	`Number of restaurants in city centre: Expected result: 2, actual result: ${numberOfRestaurantsInCityCentre}`,
+);
---
+++ Object_Built_In_Method.js
@@ -0,0 +1,129 @@
+// const obj = {
+//     name: 'John',
+//     age: 30,
+//     sayHi: function() {
+//         console.log('Hi');
+//     },
+// nestedObject: {
+//     height: 180,
+//     weight: 80,
+// }
+// }
+// // Object.keys(obj) - returns an array of keys
+// console.log(Object.keys(obj)); // [ 'name', 'age', 'sayHi', 'nestedObject' ]
+
+// // Object.values(obj) - returns an array of values
+// console.log(Object.values(obj)); // [ 'John', 30, [Function: sayHi], { height: 180, weight: 80 } ]
+
+// // Object.entries(obj) - returns an array of arrays of key-value pairs
+// console.log(Object.entries(obj)); // [ [ 'name', 'John' ], [ 'age', 30 ], [ 'sayHi', [Function: sayHi] ], [ 'nestedObject', { height: 180, weight: 80 } ] ]
+
+// EXERCISE 1
+
+// Return the keys and values of the following object
+
+
+// let capitalCities = {
+//   scotland: "Edinburgh",
+//   kenya: "Nairobi",
+//   australia: "Canberra",
+//   canada: "Ottawa",
+// };
+
+// let highScores = {
+//   55: "Alistair",
+//   100: "David",
+//   89: "Hannah",
+//   34: ["Sergi", "Frank"],
+// };
+
+// // ONLY EDIT BELOW HERE
+
+// let capitalCitiesKeys = Object.keys(capitalCities);
+// let highScoresKeys = Object.keys(highScores);
+// let capitalCitiesValues = Object.values(capitalCities);
+// let highScoresValues = Object.values(highScores);
+
+// // ONLY EDIT ABOVE HERE
+
+// console.log(capitalCitiesKeys);
+// // prints [ 'scotland', 'kenya', 'australia', 'canada' ]
+
+// console.log(highScoresKeys);
+// // prints ['34, '55', '89', '100']
+
+// console.log(capitalCitiesValues);
+// // prints [ 'Edinburgh', 'Nairobi', 'Canberra', 'Ottawa' ]
+
+// console.log(highScoresValues);
+// // prints [[ 'Sergi', 'Frank' ], 'Alistair, 'David', 'Hannah']
+
+// EXERCISE 2
+
+// You have an object of mentors and their ages below, but the keys are inconsistent. JOSH and JAMIE are uppercase, while james and Mozafar are not. You want to return an array of mentors with the names in uppercase.
+
+// You're going to have to used what we learned at the start of this lesson, and some other JavaScript methods to accomplish this task.
+
+
+let mentorsAges = {
+  james: 29,
+  JOSH: 35,
+  JAMIE: 25,
+  Mozafar: 30,
+};
+
+// // ONLY EDIT BELOW THIS LINE
+
+let mentorsNames = Object.keys(mentorsAges);
+
+let mentorsNamedUppercased = mentorsNames.map((name) => name.toUpperCase());
+
+// // ONLY EDIT ABOVE THIS LINE
+
+console.log(mentorsNamedUppercased);
+// // prints [ 'JAMES', 'JOSH', 'JAMIE', 'MOZAFAR' ]
+
+
+// EXERCISE 3
+// Objects very often contain other objects.
+
+// This exercise will give you practice in getting the keys of objects inside other objects.
+
+// Use the provided console.log statements below and follow the instructions above them.
+
+// let storeBranches = {
+//   glasgow: {
+//     manager: "Andrew",
+//     assistant: "Laura",
+//     interns: {
+//       head_intern: "Mozafar",
+//       intern: "James",
+//     },
+//   },
+
+//   edinburgh: {
+//     director: "Kelly",
+//     manager: "Sally",
+//     assistant: "Derek",
+//     interns: {
+//       head_intern: "John",
+//       intern: "Sarah",
+//     },
+//   },
+// };
+
+// ONLY EDIT BELOW THIS LINE
+
+// # 1
+// prints [ 'glasgow', 'edinburgh' ]
+// console.log(Object.keys(storeBranches));
+
+// # 2
+// prints [ 'manager', 'assistant', 'interns' ]
+// console.log(Object.keys(storeBranches.glasgow));
+
+// # 3
+// prints [ 'head_intern', 'intern' ]
+// console.log(Object.keys(storeBranches.glasgow.interns));
+
+// ONLY EDIT ABOVE THIS LINE
