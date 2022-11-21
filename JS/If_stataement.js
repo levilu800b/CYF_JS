caa6abb5297e6c27b918730a2eb7743d0167aa92
levilu800b <levilu800b@gmail.com>
2022-11-21 20:12:54 +0000

    check

--- JS/If_stataement.js
+++ JS/If_stataement.js
@@ -133,10 +133,10 @@
 
 
 
-let hungerLevel = 7;
+// let hungerLevel = 7;
 
-if (hungerLevel => 7) {
-	console.log('Time to eat!');
-} else {
-	console.log('We can eat later!');
-}
+// if (hungerLevel => 7) {
+// 	console.log('Time to eat!');
+// } else {
+// 	console.log('We can eat later!');
+// }
