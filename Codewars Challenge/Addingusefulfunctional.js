b5e43cf73ac0af11731290557da3bbf31523e863
levilu800b <levilu800b@gmail.com>
2022-12-10 12:14:55 +0000

    adds

--- Codewars Challenge/Addingusefulfunctional.js
+++ Codewars Challenge/Addingusefulfunctional.js
@@ -10,14 +10,14 @@
 // My solution:
 
 Array.range = function(start, count) {
-    return Array.apply(0, Array(count))
-        .map(function(element, index) { 
-        return index + start;  
+    return Array.apply(0, Array(count))  // apply(0, Array(count)) creates an array of length count with undefined values in it 
+        .map(function(element, index) { // map() creates a new array with the results of calling a provided function on every element in this array
+        return index + start;  // index + start is the value of the element in the new array 
         });
     };
 
 Array.prototype.sum = function() {
-    return this.reduce(function(a, b) { return a + b; }, 0);
+    return this.reduce(function(a, b) { return a + b; }, 0); // reduce() applies a function against an accumulator and each value of the array (from left-to-right) to reduce it to a single value.
 };
 
 console.log(Array.range(0, 3));
