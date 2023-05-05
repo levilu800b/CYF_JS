d279e737b4b45a90a4873f043fdc6f32fc3fdd85
levilu800b <levilu800b@gmail.com>
2023-05-05 15:55:56 +0100

    test file

---
+++ CYF - Vol/SQL Question/test-input-01.sql
@@ -0,0 +1,4 @@
+insert into visits values ('198.51.100.2', '192.0.2.241');
+insert into visits values ('203.0.113.78', '192.0.2.241');
+insert into visits values ('198.51.100.2', '203.0.113.1');
+insert into visits values ('198.51.100.2', '192.0.2.241');
